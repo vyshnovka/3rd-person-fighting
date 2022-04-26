fc49263cdba47bf9acbf3d44cd707c4d73cd1200
Vykula <viktoria.zheneral@gmail.com>
2022-04-27 00:24:14 +0300

    weapon scriptable objects created

---
+++ Assets/Scripts/Weapon/WeaponData.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponType
+{
+    Sword,
+    Scepter,
+    Bow
+}
+
+[CreateAssetMenu(fileName = "Weapon", menuName = "ScriptableObjects/WeaponData")]
+public class WeaponData : ScriptableObject
+{
+    public WeaponType type;
+    public float range;
+    public int damage;
+
+    public Sprite icon;
+}
