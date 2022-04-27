cef349f201a6f8613c13e49c4290e8a448141797
Vykula <viktoria.zheneral@gmail.com>
2022-04-27 22:55:42 +0300

    player stats class added

---
+++ Assets/Scripts/Player/StatsData.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsData
+{
+    public int health;
+
+    public StatsData()
+    {
+        health = 100;
+    }
+}
